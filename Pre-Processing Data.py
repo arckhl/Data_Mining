e1685217c2b19b4c4b77ee779dfe5605a304c6fb
arckhl <91888029+arckhl@users.noreply.github.com>
2023-03-24 22:39:37 +0700

    Update Pre-Processing Data.py

--- Pre-Processing Data.py
+++ Pre-Processing Data.py
@@ -1,21 +1,26 @@
+# Muhammad Archibald W.A.B
+# A11.2021.13578
+# A11.4606
+
+
 import numpy as ny
 import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,-1].values
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,-1].values
 
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=ny.nan, strategy='mean')
-imputer.fit(x[:, 1:3])
-x[:, 1:3] = imputer.transform(x[:, 1:3])
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(), [0])], remainder='passthrough')
-x = ny.array(ct.fit_transform(x))
+X = ny.array(ct.fit_transform(X))
 
-print(x)
+print(X)
 
-print(y)
+print(Y)
